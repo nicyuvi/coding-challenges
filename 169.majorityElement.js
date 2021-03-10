0685b5c86ad76ab18b4157b8180d756150e4f16e
Nicolis Yuvienco <nickyuvienco@gmail.com>
2021-03-10 15:27:04 -0800

    Remove comments from functions

--- 169.majorityElement.js
+++ 169.majorityElement.js
@@ -21,33 +21,23 @@
 
 // * SOLUTION:
 function majorityElement1(nums) {
-  // define half the total number of elements from our input array.
   let halfCount = nums.length / 2;
 
-  // first check if there is only one input. if so, return the input.
   if (nums.length === 1) return nums[0];
 
-  // loop through nums and declare a counter.
   for (let num of nums) {
     let counter = 0;
-    // loop through nums a second time and compare the elements from both loops.
     for (let elem of nums) {
-      // if the elements from both loops are the same value, we increment our counter.
-      // this keeps track of the number of occurrences of each element in nums
       if (num === elem) counter++;
     }
-    // once we count the number of occurences for the given element,
-    // we check if the element occurs more than half the total number of elements from nums
-    // if true, we can return that element
     if (counter > halfCount) return num;
   }
-  // otherwise there is no majority element and we return -1;
   return -1;
 }
 
 // test case:
-console.log(majorityElement1([3, 2, 3])); // 3
-console.log(majorityElement1([4, 3, 3, 5])); // -1
+console.log('Solution 1: ', majorityElement1([3, 2, 3])); // Solution 1: 3
+console.log('Solution 1: ', majorityElement1([4, 3, 3, 5])); // Solution 1: -1
 
 // * HashMap - One Loop
 
@@ -66,41 +56,28 @@ console.log(majorityElement1([4, 3, 3, 5])); // -1
 
 // * SOLUTION:
 function majorityElement2(nums) {
-  // define half the total number of elements from our input array.
   let halfCount = nums.length / 2;
 
-  // declare our map.
   const map = {};
 
-  // first check if there is only one input. If so, return the input.
   if (nums.length === 1) return nums[0];
 
-  // loop through nums and see whether to add a key to map or increment a keys' value.
-  // while we build our map, we check if our keys' value is greater than halfCount.
-  // if so, we return that key.
-
   for (let num of nums) {
-    // if num exists on map, increment its value.
     if (map.hasOwnProperty(num)) {
       map[num] += 1;
     } else {
-      // otherwise we add a new key with initial value 1.
       map[num] = 1;
     }
-    // here we check if our keys value is greater than halfCount.
     if (map[num] > halfCount) {
-      // if so, we return the key.
       return num;
     }
-    // otherwise, we continue the loop with the next element in nums.
   }
-  // if there is no majority element in the input, we return -1.
   return -1;
 }
 
 // test case:
-console.log(majorityElement2([3, 2, 3])); // 3
-console.log(majorityElement2([4, 3, 3, 5])); // -1
+console.log('Solution 2: ', majorityElement2([3, 2, 3])); // Solution 2: 3
+console.log('Solution 2: ', majorityElement2([4, 3, 3, 5])); // Solution 2: -1
 
 // * HashMap - Two Loops
 
@@ -120,37 +97,29 @@ console.log(majorityElement2([4, 3, 3, 5])); // -1
 
 // * SOLUTION:
 function majorityElement3(nums) {
-  // define half the total number of elements from our input array.
   let halfCount = nums.length / 2;
 
-  // declare our map.
   const map = {};
 
-  // loop through nums and see whether to add a key to map or increment a keys' value.
   nums.forEach((num) => {
-    // if num exists on map, increment it's value.
     if (map.hasOwnProperty(num)) {
       map[num] += 1;
     } else {
-      // otherwise we add a new key with initial value 1.
       map[num] = 1;
     }
   });
 
-  // loop through our maps' keys and evaluate which keys' value is greater than halfCount.
   for (let elem in map) {
-    // if our keys' value is greater than halfCount, we return that key.
     if (map[elem] > halfCount) {
       return elem;
     }
   }
-  // if there is no majority element in the input, we return -1.
   return -1;
 }
 
 // test case:
-console.log(majorityElement3([3, 2, 3])); // 3
-console.log(majorityElement3([4, 3, 3, 5])); // -1
+console.log('Solution 3: ', majorityElement3([3, 2, 3])); // Solution 3: 3
+console.log('Solution 3: ', majorityElement3([4, 3, 3, 5])); // Solution 3: -1
 
 // * Boyer Moore Algorithm
 
@@ -166,32 +135,24 @@ console.log(majorityElement3([4, 3, 3, 5])); // -1
 
 // * SOLUTION
 var majorityElement4 = function (nums) {
-  // initialize the first element as the candidate with count 1
   let candidate = nums[0];
   let count = 1;
-  // loop through nums starting at index 1 to check the rest of the elements
   for (let i = 1; i < nums.length; i++) {
     let elem = nums[i];
-    // if elem === candidate, we increment count
-    // otherwise decrement count
     count += elem === candidate ? 1 : -1;
 
-    // check if count === 0, this means there are is current majority element candidate
-    // if so, assign the current elem to be candidate and set count to 1
     if (count === 0) {
       candidate = elem;
       count = 1;
     }
   }
-  // at the end of the loop, return candidate
-  // this is our majority element, assuming there is one from the given input
   return candidate;
 };
-
-// test case:
-console.log(majorityElement4([3, 2, 3])); // 3
-console.log(majorityElement4([4, 3, 3, 5])); // -1
 
 // ? The problem states that we may assume that the majority element always exists in the array
 // ! IF NOT:
 // ? we can loop through again to check if candidate is actually a majority element
+
+// test case:
+console.log('Solution 4: ', majorityElement4([3, 2, 3])); // Solution 4: 3
+console.log('Solution 4: ', majorityElement4([4, 3, 3, 5])); // Solution 4: 3
