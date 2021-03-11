fcd9af169e54df159eb218289bf21b69f7ebbf92
Nicolis Yuvienco <nickyuvienco@gmail.com>
2021-03-10 17:23:27 -0800

    Add Boyer-Moore two loop solution

--- 169.majorityElement.js
+++ 169.majorityElement.js
@@ -1,6 +1,6 @@
 // * Brute Force
 
-// * PSEUDOCODE:
+// * Pseudocode:
 // define half the total number of elements from our input array.
 
 // first check if there is only one input. if so, return the input.
@@ -19,7 +19,7 @@
 
 // otherwise there is no majority element and we return -1;
 
-// * SOLUTION:
+// * Solution:
 function majorityElement1(nums) {
   let halfCount = nums.length / 2;
 
@@ -35,13 +35,13 @@ function majorityElement1(nums) {
   return -1;
 }
 
-// test case:
+// test cases:
 console.log('Solution 1: ', majorityElement1([3, 2, 3])); // Solution 1: 3
 console.log('Solution 1: ', majorityElement1([4, 3, 3, 5])); // Solution 1: -1
 
 // * HashMap - One Loop
 
-// * PSEUDOCODE:
+// * Pseudocode:
 // define half the total number of elements from our input array.
 
 // declare our map.
@@ -54,7 +54,7 @@ console.log('Solution 1: ', majorityElement1([4, 3, 3, 5])); // Solution 1: -1
 
 // if there is no majority element in the input, we return -1.
 
-// * SOLUTION:
+// * Solution:
 function majorityElement2(nums) {
   let halfCount = nums.length / 2;
 
@@ -75,13 +75,13 @@ function majorityElement2(nums) {
   return -1;
 }
 
-// test case:
+// test cases:
 console.log('Solution 2: ', majorityElement2([3, 2, 3])); // Solution 2: 3
 console.log('Solution 2: ', majorityElement2([4, 3, 3, 5])); // Solution 2: -1
 
 // * HashMap - Two Loops
 
-// * PSEUDOCODE:
+// * Pseudocode:
 // define half the total number of elements from our input array .
 
 // declare our map.
@@ -95,7 +95,7 @@ console.log('Solution 2: ', majorityElement2([4, 3, 3, 5])); // Solution 2: -1
 
 // if there is no majority element in the input, we return -1.
 
-// * SOLUTION:
+// * Solution:
 function majorityElement3(nums) {
   let halfCount = nums.length / 2;
 
@@ -117,13 +117,13 @@ function majorityElement3(nums) {
   return -1;
 }
 
-// test case:
+// test cases:
 console.log('Solution 3: ', majorityElement3([3, 2, 3])); // Solution 3: 3
 console.log('Solution 3: ', majorityElement3([4, 3, 3, 5])); // Solution 3: -1
 
-// * Boyer Moore Algorithm
+// * Boyer-Moore Algorithm
 
-// * PSEUDOCODE
+// * Pseudocode
 // initialize the first element as the candidate with count 1
 // loop through nums starting at index 1 to check the rest of the elements
 // if elem === candidate, we increment count
@@ -133,8 +133,8 @@ console.log('Solution 3: ', majorityElement3([4, 3, 3, 5])); // Solution 3: -1
 // at the end of the loop, return candidate
 // this is our majority element, assuming there is one from the given input
 
-// * SOLUTION
-var majorityElement4 = function (nums) {
+// * Solution - One Loop
+function majorityElement4(nums) {
   let candidate = nums[0];
   let count = 1;
   for (let i = 1; i < nums.length; i++) {
@@ -146,13 +146,74 @@ var majorityElement4 = function (nums) {
       count = 1;
     }
   }
+
   return candidate;
-};
+}
+
+// test cases:
+console.log('Solution 4: ', majorityElement4([3, 2, 3])); // Solution 4: 3
+console.log('Solution 4: ', majorityElement4([4, 3, 3, 5])); // Solution 4: 3
+// ! in this case we would have to loop through a second time to check if 3 is actually a majority element.
 
 // ? The problem states that we may assume that the majority element always exists in the array
 // ! IF NOT:
 // ? we can loop through again to check if candidate is actually a majority element
 
-// test case:
-console.log('Solution 4: ', majorityElement4([3, 2, 3])); // Solution 4: 3
-console.log('Solution 4: ', majorityElement4([4, 3, 3, 5])); // Solution 4: 3
+// * Check if the candidate is actually a majority element
+
+// * Pseudocode
+// initialize our count at 0
+// we loop through our input and check if each element === candidate
+// if so, we increment count
+// we check if count is greater than half the total number of elements from our input array
+// if so, our candidate is a majority element
+// if no, our candidate is not a majority element
+
+// * Solution
+function isMajorityElem(nums, candidate) {
+  let halfCount = nums.length / 2;
+  let count = 0;
+  for (let elem of nums) {
+    if (elem === candidate) count++;
+    if (count > halfCount) return true;
+  }
+  return false;
+}
+
+// test cases:
+console.log('Is candidate a majority element?', isMajorityElem([3, 2, 3], 3)); // true
+console.log(
+  'Is candidate a majority element?',
+  isMajorityElem([4, 3, 3, 5], 3),
+); // false
+
+// * Boyer-Moore Algorithm Two Loops Solution:
+
+function majorityElement5(nums) {
+  let candidate = nums[0];
+  let count = 1;
+
+  let halfCount = nums.length / 2;
+  let checkCount = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    let elem = nums[i];
+    count += elem === candidate ? 1 : -1;
+
+    if (count === 0) {
+      candidate = elem;
+      count = 1;
+    }
+  }
+
+  for (let elem of nums) {
+    if (elem === candidate) checkCount++;
+    if (checkCount > halfCount) return candidate;
+  }
+  return -1;
+}
+
+// test casess:
+console.log('Solution 5: ', majorityElement5([3, 2, 3])); // Solution 4: 3
+console.log('Solution 5: ', majorityElement5([4, 3, 3, 5])); // Solution 4: -1
+// * here our solution correctly prints out -1 to signify there is no majority element
